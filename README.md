46f6b3786f53203e032bd1cee08fbba576274e0b
JoshBuilds <4267130+JoshBuilds@users.noreply.github.com>
2020-05-23 17:00:06 -0500

    First version

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# homekit-led-strip
+# Raspberry Pi - Homekit LED Strip
+
+## About
+This repo pairs with my video on using the Raspberry Pi to control a single color LED strip with Homekit.
+
+## How to use
+
+### BasementMaker Raspberry Pi Configurator (Recommended) > [GitHub Repo](https://github.com/basementmaker/raspberrypi-configurator)
+
+```
+wget https://raw.githubusercontent.com/basementmaker/raspberrypi-configurator/master/pi-config.sh
+chmod +x pi-config.sh
+./pi-config.sh
+```
+then
+
+Select: Video Projects
+then select: Raspberry Pi - HomeKit Controlled Single Color LED Strip [May 2020]
+
+### Manually
+- Install Node.js, version 12 preferred > [Instructions](https://github.com/nodesource/distributions/blob/master/README.md)
+- Install PiGPIO > [Instructions](https://github.com/fivdi/pigpio)
+
+and then these commands from the Raspberry Pi:
+- git clone https://github.com/basementmaker/raspberrypi-homekit-led-strip.git
+- cd raspberrypi-homekit-led-strip
+- npm install
+- sudo npm start
---
+++ index.js
@@ -0,0 +1,68 @@
+const gpio = require('pigpio').Gpio;
+const hap = require("hap-nodejs");
+
+const Accessory = hap.Accessory;
+const Characteristic = hap.Characteristic;
+const CharacteristicEventTypes = hap.CharacteristicEventTypes;
+const Service = hap.Service;
+
+const accessoryUuid = hap.uuid.generate("basementmaker.projects.ledstrip");
+const accessory = new Accessory("LED Strip Accesssory", accessoryUuid);
+
+const onCharacteristic = lightService.getCharacteristic(Characteristic.On);
+const brightnessCharacteristic = lightService.getCharacteristic(Characteristic.Brightness);
+
+const lightService = new Service.Lightbulb("LED Strip");
+
+var showLogging = true;
+var LEDstripStatusIsOn = false;
+var currentLEDbrightness = 0;
+var ledStripGPIOpin = new gpio(17, {mode: gpio.OUTPUT});
+
+ledStripGPIOpin.pwmWrite(0);
+
+onCharacteristic.on(CharacteristicEventTypes.GET, callback => {
+  if (showLogging) { console.log("Is LED Strip On?: " + LEDstripStatusIsOn); }
+  callback(undefined, LEDstripStatusIsOn);
+});
+
+onCharacteristic.on(CharacteristicEventTypes.SET, (value, callback) => {
+  if (showLogging) { console.log("Setting LED Strip On: " + value); }
+  if ( value == true && LEDstripStatusIsOn == false) {
+    if ( currentLEDbrightness == 0 ) {
+      ledStripGPIOpin.pwmWrite(255);
+    } else {
+      ledStripGPIOpin.pwmWrite(currentLEDbrightness);
+    }
+  } else if ( value == false ) {
+    ledStripGPIOpin.pwmWrite(0);
+  } else {
+    // do nothing
+  }
+  LEDstripStatusIsOn = value;
+  callback();
+});
+
+brightnessCharacteristic.on(CharacteristicEventTypes.GET, (callback) => {
+  if (showLogging) { console.log("Current Strip brightness level?: " + currentLEDbrightness); }
+  callback(undefined, currentLEDbrightness);
+});
+
+brightnessCharacteristic.on(CharacteristicEventTypes.SET, (value, callback) => {
+  if (showLogging) { console.log("Setting Strip brightness level to: " + value); }
+  var val = parseInt(255*(value/100), 10);
+  ledStripGPIOpin.pwmWrite(val);
+  currentLEDbrightness = Math.ceil((val/255)*100);
+  callback();
+});
+
+accessory.addService(lightService);
+
+accessory.publish({
+  username: "BB:00:00:00:00:01",
+  pincode: "000-00-123",
+  port: 47129,
+  category: hap.Categories.LIGHTBULB
+});
+
+if (showLogging) { console.log("Accessory setup finished!"); }
